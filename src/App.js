0c58ba80737b54d97832a21d5622b8cf1b68d4d6
chaudeveloper <ms_michanguyen@yahoo.com>
2022-04-09 16:05:18 -0400

    logo, title and footter, color scheme set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id='title'>Chau Nguyen</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+function Footer() {
+    return (
+        <footer>
+           <a href='https://github.com/ChauDeveloper' target="_blank"><i class="fa-brands fa-github"></i></a>
+           <a href='https://www.linkedin.com/in/chau-nguyen-50677818/' target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+           <a href='https://www.instagram.com/chaulovesmusic/' target="_blank"><i class="fa-brands fa-instagram-square"></i></a> 
+        </footer>
+    );
+}
+
+export default Footer;
