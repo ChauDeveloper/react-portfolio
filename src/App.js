b8862d9bed4991ddbb3581e52c68bff8d1e10d3d
chaudeveloper <ms_michanguyen@yahoo.com>
2022-04-09 16:17:02 -0400

    navbar started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar';
 import Footer from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 id='title'>Chau Nguyen</h1>
       <Footer />
     </div>
---
+++ src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+
+function Navbar() {
+    return (
+        <ul>
+            <li><a href="default.asp">About Me</a></li>
+            <li><a href="news.asp">Portfolio</a></li>
+            <li><a href="contact.asp">Resume</a></li>
+            <li><a href="about.asp">Contact</a></li>
+        </ul>
+    );
+}
+
+export default Navbar;
