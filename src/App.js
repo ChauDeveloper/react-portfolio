77ad7dcc8efa401362b2855d1d8a0415ddf5dadb
chaudeveloper <ms_michanguyen@yahoo.com>
2022-04-10 09:33:22 -0400

    fixing some typo on app.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
       <Navbar />
       <Switch>
-                <Route exact path="/" component={About} exact={true}/>
+                <Route exact={true} path="/" component={About} />
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/resume" component={Resume} />
                 <Route exact path="/contact" component={Contact} />
