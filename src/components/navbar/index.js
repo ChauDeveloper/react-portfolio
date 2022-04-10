f54d3b88133da81be1138c3bd874ee01b78e0b6b
chaudeveloper <ms_michanguyen@yahoo.com>
2022-04-10 01:33:09 -0400

    linking all pages together and navbar link accordingly to pages

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './style.css'
 
-function Navbar() {
+function Navbar(props) {
+    const {
+        aboutSelected,
+        setAboutSelected
+    } = props;
+
     return (
         <ul id="navbar">
-            <li><a href="/">About Me</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/resume">Resume</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/" >About Me</a></li>
+            <li><a href="/portfolio" >Portfolio</a></li>
+            <li><a href="/resume" >Resume</a></li>
+            <li><a href="/contact" >Contact</a></li>
         </ul>
     );
 }
